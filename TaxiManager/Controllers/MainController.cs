3da5b1a474039398ea85e2c1b33241801c49a4ee
DrivePotato <oxman.joshua@gmail.com>
2015-03-03 19:19:53 -0500

    Test sur index du taxi.

--- TaxiManager/Controllers/MainController.cs
+++ TaxiManager/Controllers/MainController.cs
@@ -3,37 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web.Mvc;
 
 namespace TaxiManager.Controllers
 {
-    public class MainController : ApiController
+    public class MainController : Controller
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        public ActionResult Index()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            return View("~/Views/Taxi/Index.cshtml");
         }
     }
 }
