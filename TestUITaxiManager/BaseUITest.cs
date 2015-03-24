fa224356bed0ff22db809bdc286321e353060c29
DrivePotato <oxman.joshua@gmail.com>
2015-03-24 19:35:11 -0400

    Projet de test UI

---
+++ TestUITaxiManager/BaseUITest.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace TestUITaxiManager
+{
+    public class BaseUITest
+    {
+        public IE browser;
+        public string rootURL;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+            rootURL = ConfigurationManager.AppSettings["RootURL"];
+            // hide IE browser during tests
+            Settings.Instance.MakeNewIeInstanceVisible = false;
+            browser = new IE(rootURL + "Account/LogOn");
+            browser.WaitForComplete();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            if (browser != null)
+            {
+                browser.ForceClose();
+            }
+        }
+    }
+}
