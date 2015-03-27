b93ec8fc0e1165f42c782ea713281d2aa640c769
DrivePotato <oxman.joshua@gmail.com>
2015-03-26 23:28:20 -0400

    - Ajout Test UI Projet-STP-TC-1 - Enlevé alert() lorsque l'agence entre une adresse

---
+++ TaxiManagerTestUI/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace TaxiManagerTestUI
+{
+    [RequiresSTA]
+    class BaseTest
+    {
+
+        protected IE LoginAgency()
+        {
+            IE browser = new IE();
+
+            browser.GoTo("http://localhost:53832/Account/Login");
+            browser.WaitForComplete();
+
+            browser.TextField(Find.ByName("UserName")).TypeText("Agency");
+            browser.TextField(Find.ByName("Password")).TypeText("Agency123[");
+
+            browser.Button(Find.ByValue("Connexion")).Click();
+            browser.WaitForComplete();
+
+            return browser;
+        }
+    }
+}
--- TaxiManagerTestUI/TestExigenceFonctionnel5.cs
+++ TaxiManagerTestUI/TestExigenceFonctionnel5.cs
@@ -10,18 +10,47 @@ using WatiN.Core;
 namespace TaxiManagerTestUI
 {
     [TestFixture, RequiresSTA]
-    class TestExigenceFonctionnel5
+    class TestExigenceFonctionnel5: BaseTest
     {
 
         [Test]
-        public static void Test_Pas_Rapport()
+        public void Test_StartPage()
         {
             using (WatiN.Core.IE browser = new IE())
             {
-                browser.GoTo("http://localhost:53832");
+                browser.GoTo("http://localhost:53832/");
                 browser.WaitForComplete();
                 Assert.True(browser.Text.Contains("Taxi Manager"));
             }      
+        }
+
+        [Test]
+        public void Test_Projet_STP_TC_1()
+        {
+            using (IE browser = this.LoginAgency())
+            {
+                // Va à la page de l'agency
+                browser.GoTo("http://localhost:53832/Agency");
+                browser.WaitForComplete();
+                                
+                // Remplie les textfield
+                browser.TextField(Find.ByName("Adress")).TypeText("1100 rue Notre-Dame Ouest, Montréal");
+                browser.TextField(Find.ByName("FirstName")).TypeText("Blos");
+                browser.TextField(Find.ByName("LastName")).TypeText("Joe");
+
+                // Catch l'adresse dans le systeme
+                var searchAdress = browser.TextField(Find.ByName("Adress")).Text;
+                Assert.True(searchAdress.Equals("1120 Rue Notre Dame Ouest, Montréal, QC H3C 0J8, Canada"));
+
+                // Click nouveau client
+                browser.Button(Find.ByValue("Nouveau client")).Click();
+                browser.WaitForComplete();
+
+                // Les champs sont supposé être effacé.
+                Assert.True(browser.TextField(Find.ByName("FirstName")).Text == null);
+                Assert.True(browser.TextField(Find.ByName("LastName")).Text == null);
+                Assert.True(browser.TextField(Find.ByName("Adress")).Text == null);
+            }
         }
     }
 }
