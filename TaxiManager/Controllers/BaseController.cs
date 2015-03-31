5e3bc276c55fa2e8cb13f771b2bf18fbc710f086
Mathieu Legault <mathieu.legault.1@ens.etsmtl.ca>
2015-03-31 15:41:41 -0400

    Added SetTaxiInSession

--- TaxiManager/Controllers/BaseController.cs
+++ TaxiManager/Controllers/BaseController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Newtonsoft.Json;
 using TaxiManager.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
 
 namespace TaxiManager.Controllers
 {
@@ -42,6 +45,62 @@ namespace TaxiManager.Controllers
             // Add customers to session 
             Session["Customers"] = JsonConvert.SerializeObject(customers);
         }
+
+        public void SetTaxiInSession()
+        {
+            //for now so I can test things...
+            List<ApplicationUser> taxiUsers = new List<ApplicationUser>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "Select * From AspNetUsers WHERE [Discriminator] = N'ApplicationUser'";
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        using (SqlCommand commandGetRole = new SqlCommand())
+                        {
+                            commandGetRole.Connection = connection;
+                            commandGetRole.CommandText = "Select * From aspNetUserRoles Where [UserId] = @UserId and [RoleId] = N'5644ca88-4f79-4386-b69e-ec0774202aad'";
+                            commandGetRole.Parameters.AddWithValue("@UserId", (string)reader["Id"]);
+                            SqlDataReader results = commandGetRole.ExecuteReader();
+                            if (results.HasRows)
+                            {
+                                // c'est un taxi
+                                ApplicationUser user = new ApplicationUser();
+                                user.Adress = reader["Adress"].ToString();
+                                user.CurrentAdress = reader["CurrentAdress"].ToString();
+                                user.CurrentStatus = (Status)reader["CurrentStatus"];
+                                user.Id = reader["Id"].ToString();
+                                user.UserName = reader["UserName"].ToString();
+                                var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(user.Adress));
+
+                                var request = WebRequest.Create(requestUri);
+                                var response = request.GetResponse();
+                                var xdoc = new System.Xml.XmlDocument();
+                                xdoc.Load(response.GetResponseStream());
+
+                                var resp = xdoc["GeocodeResponse"];
+                                var result = resp["result"];
+                                var geo = result["geometry"];
+                                var loc = geo["location"];
+                                var lat = loc["lat"].InnerText;
+                                var lng = loc["lng"].InnerText;
+                                user.Latitude = lat;
+                                user.Longitude = lng;
+                                taxiUsers.Add(user);
+                            }
+                        }
+                    }
+                }
+                connection.Close();
+                Session["Taxis"] = JsonConvert.SerializeObject(taxiUsers);
+            }
+        }
+
 
         public LatLng LatLngFromAddress(string address)
         {
